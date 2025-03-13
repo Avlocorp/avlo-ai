64dfe85d969c1b41491c43e123bf5b1d7d5fe4ce
nurulloh-ubaydullayev <nurulloh.ubaydullayev2004@gmail.com>
2025-03-13 11:09:31 +0500

    feat: calls history

--- src/modules/client-history/index.tsx
+++ src/modules/client-history/index.tsx
@@ -1,32 +1,66 @@
-import { Timeline } from "antd";
+import { Button, Timeline } from "antd";
 import AudioFile from "assets/icons/mp3-icon.png";
+import { Cpu, Download, Eye } from "lucide-react";
+import { useMemo } from "react";
+import { Link } from "react-router-dom";
+import {
+  useAnalyzeAudioMutation,
+  useGetClientsByPhoneNumberQuery,
+} from "services/api/audios/audios.api";
 
 const ClientsHistory = () => {
+  const [analyzeAudios] = useAnalyzeAudioMutation();
+  const { data } = useGetClientsByPhoneNumberQuery({
+    phone: "998331300529",
+  });
+
+  const history = useMemo(() => {
+    return data?.data.map((item) => {
+      return {
+        dot: <img src={AudioFile} alt="" width={40} height={40} />,
+        children: (
+          <div className="flex gap-8 pl-5">
+            <div className="w-[25%]">
+              <h3>
+                {item.operator.name} {item.operator.last_name}
+              </h3>
+              <p>{item.call_date}</p>
+            </div>
+
+            <div className="flex gap-3 items-center">
+              <Link to={item.download_link} download>
+                <Button
+                  type="text"
+                  icon={<Download />}
+                  className={`hover:text-white hover:bg-zinc-800 !text-[#5B9BEC]`}
+                />
+              </Link>
+              <Button
+                icon={<Cpu />}
+                type="text"
+                className={item.analysed ? "text-[#4A554F]" : "text-[#5B9BEC]"}
+                onClick={() => {
+                  analyzeAudios(item.id.toString());
+                }}
+              ></Button>
+              <Link
+                to={item.analysed ? `/pm/call-center/audio/${item.id}` : ""}
+              >
+                <Eye color={item.analysed ? "#5B9BEC" : "#4A554F"} />
+              </Link>
+            </div>
+          </div>
+        ),
+      };
+    });
+  }, [data]);
+
   return (
     <section className="p-6">
       <h1 className="text-3xl font-semibold text-white">Clients History</h1>
       <p className="mb-[100px]">Here we will show the history of clients</p>
 
-      <Timeline
-        items={[
-          {
-            dot: <img src={AudioFile} alt="" width={40} height={40} />,
-            children: <p className="pl-5">Solve initial network problems</p>,
-          },
-          {
-            dot: <img src={AudioFile} alt="" width={40} height={40} />,
-            children: <p className="pl-5">Solve initial network problems</p>,
-          },
-          {
-            dot: <img src={AudioFile} alt="" width={40} height={40} />,
-            children: <p className="pl-5">Solve initial network problems</p>,
-          },
-          {
-            dot: <img src={AudioFile} alt="" width={40} height={40} />,
-            children: <p className="pl-5">Solve initial network problems</p>,
-          },
-        ]}
-      />
+      <Timeline items={history} className="ml-6" />
     </section>
   );
 };
--- src/services/api/audios/audios.types.ts
+++ src/services/api/audios/audios.types.ts
@@ -64,3 +64,46 @@ export interface AnalysedCalls {
   unsuccessfully_calls: number;
   total_calls: number;
 }
+
+// -----------------
+export type ClientsHistory = ResponseDataWithPagination<Client>;
+
+export interface Client {
+  id: number;
+  operator_id: number;
+  name: string;
+  audio_id: string;
+  phone: string;
+  download_link: string;
+  detail_link: string;
+  size: string;
+  file_upload_date: string;
+  created_at: string;
+  updated_at: string;
+  operator: Operator;
+  call_date: string;
+  analysed: boolean;
+  status?: string;
+  analysed_data?: AnalysedData;
+}
+
+export interface AnalysedData {
+  conversation_title: string;
+  main_contents: string;
+  essential_points: string[];
+  caller_emotion: string;
+  overall_performance_score: number;
+  communication_skills_score: number;
+  problem_handling_score: number;
+  customer_management_score: number;
+  protocol_adherence_score: number;
+  resolution_status: string;
+  resolution_quality: string;
+  agent_development_opportunities: string[];
+  process_improvement_suggestions: string[];
+  resource_or_tool_recommendations: string[];
+  notable_techniques: string[];
+  successful_interaction_strategies: string[];
+  examples_for_training: string[];
+  conclusion: string;
+}
